d9a44c9d26ad77a46ec25420a66e6cbf7a00082e
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-28 23:24:22 -0800

    update js

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ const Manager = require("./library/manager");
 const Engineer = require("./library/engineer");
 const Intern = require("./library/intern");
 let myTeam = [];
+
+// team  initiation
+function askUser() {
+    return inquirer.prompt([{
+            type: "input",
+            message: "Your Team Name",
+            name: "teamName",
+        }, ])
+        .then(function(data) {
+            const teamName = data.teamName;
+            myTeam.push(teamName);
+            addManager();
+        });
+}
